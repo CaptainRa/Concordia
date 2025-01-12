3ca0d99bf117e00383177da7f6fb88a17bdf549d
Falydra <daffa.khususedukasi@gmail.com>
2025-01-12 11:34:39 +0700

    feat: hero section

--- resources/js/Components/Banner.tsx
+++ resources/js/Components/Banner.tsx
@@ -1,10 +1,13 @@
 import React, {useRef} from 'react';
 import {motion, useScroll, useTransform, useInView} from 'framer-motion';
+import { Button } from './ui/button';
+import {Link} from 'react-scroll';
 
 
 const Banner = () => {
 
     const ref = useRef(null);
+     const isInView = useInView(ref, { once: true, margin: '0px 0px -5% 0px' });
     const { scrollYProgress } = useScroll({
         target: ref,
         offset: ["start start", "end start"],
@@ -12,8 +15,6 @@ const Banner = () => {
 
     const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100"]);
     const Text = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
-
-    const isInView = useInView(ref, {once: true});
 
     return (
         <div 
@@ -43,6 +44,26 @@ const Banner = () => {
             >
               
             </div>
+            <motion.div
+                className='w-full h-full items-center flex flex-col justify-end'
+                initial={{ opacity: 0, y: -200 }}
+                animate={{ opacity: isInView ? 1 : 0, y: isInView ? 100 : -200 }}
+                transition={{ duration: 0.9, ease: [0.17, 0.55, 0.55, 1], delay: 0.05 }}
+            >
+                <Link to='game-section' smooth={true} delay={100}>
+
+                    <Button
+                        className="animate-bounce bg-transparent backdrop-blur-xl border border-dark-fg w-[170px] h-[45px] text-white px-8 py-4 rounded-3xl z-4 relative"
+                        variant={"ghost"}
+                       
+                    >
+                        Get Started
+                    </Button>
+                </Link>
+                <svg className="animate-bounce w-6 h-6">
+                
+                </svg>
+            </motion.div>
           
         </div>
     );
