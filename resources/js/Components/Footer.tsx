7d6e603e56fd27a1f5e74efbe3b301be74c752f9
Falydra <daffa.khususedukasi@gmail.com>
2025-01-13 21:08:39 +0700

    feat: added section

--- resources/js/Components/Footer.tsx
+++ resources/js/Components/Footer.tsx
@@ -1,4 +1,6 @@
+
 import { CiMail } from "react-icons/ci";
+import { Link } from "react-scroll";
 
 export default function Footer() {
     return (
@@ -18,8 +20,41 @@ export default function Footer() {
                 </div>
 
             </div>
-            <div className="w-full flex flex-row items-center justify-center text-white">
-                <h1> Concordia</h1>
+            <div className="w-full flex flex-col items-start justify-center text-white">
+                <h1> Our Services</h1>
+                <Link to='game-section' smooth={true} delay={100} className="cursor-pointer hover:text-blue-300"> 
+                    <h1>
+                        
+                        Games
+                    </h1>
+                </Link> 
+                <Link to='schedule-section' smooth={true} delay={100} className="cursor-pointer hover:text-blue-300">
+                    <h1>
+                        
+                        Schedule
+                    </h1>
+                </Link>
+                <Link to='food-section' smooth={true} delay={100} className="cursor-pointer hover:text-blue-300">
+                    <h1>
+                        
+                        Food
+                    </h1>
+                </Link>
+                <Link to='book-section' smooth={true} delay={100} className="cursor-pointer hover:text-blue-300">
+                    <h1>
+                        
+                        Book
+                    </h1>
+                </Link>
+                <Link to='music-section' smooth={true} delay={100} className="cursor-pointer hover:text-blue-300">
+                    <h1>
+                        
+                        Music
+                    </h1>
+                </Link>
+                {/* <h1> Make Schedule</h1>
+                <h1> Picking Food</h1> */}
+
 
             </div>
         </div>
