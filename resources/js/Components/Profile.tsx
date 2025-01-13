7140768bead201f09233921cff9438ec017753a2
Falydra <daffa.khususedukasi@gmail.com>
2025-01-13 20:55:47 +0700

    feat: added section

---
+++ resources/js/Components/Profile.tsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+    return (
+        <div className="flex gap-8 w-full min-h-screen flex-row items-center justify-start py-20 bg-dark-bg" id='profile-section'>
+
+        </div>
+    );
+}
