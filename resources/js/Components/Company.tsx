4af1e41ebdfe6137be1ebf61da2aab82fcc8f77c
Falydra <daffa.khususedukasi@gmail.com>
2025-01-15 23:07:04 +0700

    fix: overflow

--- resources/js/Components/Company.tsx
+++ resources/js/Components/Company.tsx
@@ -4,7 +4,7 @@ export default function Company() {
     return (
         <>
             <h1 className="text-2xl font-semibold"> Concordia</h1>
-            <h5 className="text-sm font-thin text-balance"> We're inspired by something that bring us <br/> forget about small task or else. So, we decide  to create this our personal website <br/> to prevent loss scheduling and login streak of puzzle games.  </h5>
+            <h5 className="text-xs font-thin text-balance"> We're inspired by something that bring us <br/> forget about small task or else. So, we decide  to create this our personal website <br/> to prevent loss scheduling and login streak of puzzle games.  </h5>
             <div className="flex w-full flex-row items-center justify-start text-white mt-4 gap-2">
 
                 <CiMail className="text-white w-5 h-5" />
