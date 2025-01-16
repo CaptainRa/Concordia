4337075ae89c443fda80308093e4d3d7c5b1efcc
Falydra <daffa.khususedukasi@gmail.com>
2025-01-17 00:25:06 +0700

    fix: carousel

--- resources/js/Components/CenteredCarousel.tsx
+++ resources/js/Components/CenteredCarousel.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
+import './CenteredCarousel.css';
 
 const CenteredCarousel = () => {
     const games = [
@@ -12,9 +13,8 @@ const CenteredCarousel = () => {
         { name: 'Memory Game', image: 'images/Image Vector.png' }
     ];
 
-   
     return (
-        <div className="w-full px-12 py-12 flex flex-col "> {/* Added padding to match the "Welcome to Concordia" text */}
+        <div className="w-5/6 px-12 h-full py-12 flex flex-col"> 
             <Splide
                 options={{
                     type: 'loop',
@@ -22,32 +22,20 @@ const CenteredCarousel = () => {
                     focus: 'center',
                     gap: '4px',
                     height: '300px',
-                    
-                
-                    
                     interval: 3000,
                     pauseOnHover: true,
                     pauseOnFocus: true,
-                    // breakpoints: {
-                    //     768: {
-                    //         perPage: 1,
-                    //         height: '250px',
-                    //     },
-                    //     1024: {
-                    //         perPage: 2,
-                    //         height: '250px',
-                    //     },
-                    // },
                 }}
                 aria-label="Game Carousel"
             >
                 {games.map((game, index) => (
                     <SplideSlide key={index}>
-                        <div className="w-[195px] h-[250px] flex flex-col ml-14 self-center items-start justify-start  bg-dark-accent rounded-2xl">
+                        <div className="carousel-card bg-dark-accent rounded-2xl transition-transform duration-300 ease-in-out transform hover:scale-105">
                             <div
-                                className="h-3/5 bg-center rounded-t-2xl bg-white w-full"
+                                className="carousel-card-image bg-cover bg-center rounded-t-2xl"
+                                style={{ backgroundImage: `url(${game.image})` }}
                             />
-                            <h2 className="text-dark-bg text-xl font-semibold mt-4">{game.name}</h2>
+                            <h2 className="carousel-card-title text-dark-bg text-xl font-semibold mt-4">{game.name}</h2>
                         </div>
                     </SplideSlide>
                 ))}
