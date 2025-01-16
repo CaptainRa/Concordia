07a2359fd2c1af7e32b91c71bd4859a796633e56
Falydra <daffa.khususedukasi@gmail.com>
2025-01-16 23:15:56 +0700

    fix: on hover center

--- resources/js/Components/CenteredCarousel.tsx
+++ resources/js/Components/CenteredCarousel.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
-import 'css/CenteredCarousel.css'
 
 const CenteredCarousel = () => {
     const games = [
@@ -13,29 +12,40 @@ const CenteredCarousel = () => {
         { name: 'Memory Game', image: 'images/Image Vector.png' }
     ];
 
+   
     return (
-        <div className="w-full  items-center py-12 flex flex-col "> {/* Added padding to match the "Welcome to Concordia" text */}
+        <div className="w-full px-12 py-12 flex flex-col "> {/* Added padding to match the "Welcome to Concordia" text */}
             <Splide
                 options={{
                     type: 'loop',
                     perPage: 3,
                     focus: 'center',
-                    gap: '12px',
-                    height: '255px',
-                    width: '900px',
+                    gap: '4px',
+                    height: '300px',
+                    
+                
                     
                     interval: 3000,
                     pauseOnHover: true,
                     pauseOnFocus: true,
+                    // breakpoints: {
+                    //     768: {
+                    //         perPage: 1,
+                    //         height: '250px',
+                    //     },
+                    //     1024: {
+                    //         perPage: 2,
+                    //         height: '250px',
+                    //     },
+                    // },
                 }}
                 aria-label="Game Carousel"
             >
                 {games.map((game, index) => (
                     <SplideSlide key={index}>
-                        <div className="w-[195px] h-[250px] flex flex-col items-center justify-center  bg-dark-accent rounded-2xl">
+                        <div className="w-[195px] h-[250px] flex flex-col ml-14 self-center items-start justify-start  bg-dark-accent rounded-2xl">
                             <div
-                                className="h-48 bg-cover bg-center rounded-t-2xl"
-                                style={{ backgroundImage: `url(${game.image})` }}
+                                className="h-3/5 bg-center rounded-t-2xl bg-white w-full"
                             />
                             <h2 className="text-dark-bg text-xl font-semibold mt-4">{game.name}</h2>
                         </div>
