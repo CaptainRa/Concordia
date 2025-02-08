e3bbe759d739f5550935751e07bfe8b1a3cd9535
Falydra <daffa.khususedukasi@gmail.com>
2025-02-08 20:44:07 +0700

    fix: fetch error

--- resources/js/Components/SpotifyPlayer.tsx
+++ resources/js/Components/SpotifyPlayer.tsx
@@ -69,15 +69,16 @@ const SpotifyPlayerComponent: React.FC = () => {
           </div>
         ))} */}
         <iframe
-  title="Spotify Embed: Recommendation Playlist "
-  src={`https://open.spotify.com/embed/playlist/52R7WKopcydAXeRj5cr3tS?utm_source=generator&theme=0`}
-  width="100%"
-  height="100%"
-  style={{ minHeight: '360px', minWidth: '100vh' }}
-  frameBorder="0"
-  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-  loading="lazy"
-/>
+          title="Spotify Embed: Recommendation Playlist "
+          src={`https://open.spotify.com/embed/playlist/52R7WKopcydAXeRj5cr3tS?utm_source=generator&theme=0`}
+          width="100%"
+          height="100%"
+          style={{ minHeight: '360px', minWidth: '1000px' }}
+          
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+          
+        />
       </div>
 
       {currentTrack && token && (
